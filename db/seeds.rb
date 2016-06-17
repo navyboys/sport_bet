a093aefaaf742a49599a35718c81f0b3aac418a2
maxahn <ahnmaximus@gmail.com>
2016-06-17 10:54:58 -0700

    error in seed file fixed where one user had nil points

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 class AddInitialUsers < ActiveRecord::Migration
 
   user_names = ["Richard", "Natalia", "Ben", "Max", "Matt", "Eamonn", "Vanessa", "Deepak", "Alexandra","Juliana", "Terrance"]
-  bet_points = [1500, 1500, 1500, 150000000, 200, 300, 400, 500, 600, 0]
+  bet_points = [1500, 1500, 1500, 150000000, 200, 300, 1, 500, 600, 0, 2000]
   
   email = "@sports_bet.com"
   i = 0 
@@ -9,5 +9,7 @@ class AddInitialUsers < ActiveRecord::Migration
     User.create!(username: name, password_hash: 'password', email: name + email, points: bet_points[i])  
     i += 1
   end
+  
+  #Following.create!(follower:1, followee: 2)
 
 end
