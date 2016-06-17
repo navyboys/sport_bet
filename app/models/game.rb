824e7793663237d3505d2fb8311bf3451d695db2
maxahn <ahnmaximus@gmail.com>
2016-06-17 15:43:13 -0700

    method seems to be working but further testing required

--- app/models/game.rb
+++ app/models/game.rb
@@ -25,6 +25,44 @@ class Game < ActiveRecord::Base
     bets.reduce(0) {|m, bet| m + bet.points}
   end
 
+  def resolve_bet(winning_team) #assumes winning_team will be be <Team> or is it <GameTeam>?, if nil it was a tie else they were the winning team
+    if completed?
+      return  # already did this book-keeping, don't redo it
+    end
+
+    if winning_team
+      game_team_a = self.game_teams.first
+      game_team_b = self.game_teams.last
+      winning_game_team_id = game_team_a.team_id == winning_team.id ? game_team_a.id : game_team_b.id  
+      winning_bets = self.bets.where(game_team_id: winning_game_team_id) 
+      #binding.pry
+      losing_bets = self.bets.where.not(game_team_id: winning_game_team_id)
+      winning_bet_pool = winning_bets.reduce(0) {|m, bet| m + bet.points}   
+      winning_bets.each do |bet|          #sets bet's profit_points
+        decimal_percentage = bet.points.to_f / winning_bet_pool.to_f
+        winnings = (pool * decimal_percentage).floor
+        bet.profit_points = winnings
+        bet.save!
+        bet.user.points += winnings  #sets user's points winnings from bet
+        bet.user.save!
+      end
+      losing_bets.each do |bet|
+        bet.profit_points = 0
+        bet.save!
+      end
+    else
+      self.bets.each do |bet|     #everyone gets their points back and 'profit points' are set to bet point amount
+        bet.profit_points = bet.points
+        bet.save!
+        bet.user.points = bet.points
+        bet.user.save!
+      end
+    end
+      self.status = 'Final' 
+      self.save!
+    # figure out if we just refund points.  either no winner, or all bets on one side
+  end
+
   private
 
   def winner_count
@@ -39,36 +77,26 @@ class Game < ActiveRecord::Base
     bet.game.bets.select { |b| b.game_team.result == other_hand }.count
   end
 
-  def resolve_bet
-    return unless completed?
+# def resolve_bet
+#   return unless completed?
 
-    bets.each do |bet|
-      bet_result = bet.game_team.result
+#   bets.each do |bet|
+#     bet_result = bet.game_team.result
 
-      # bet points back to user
-      if status == 'Canceled' ||                     # Game Canceled
-        (status == 'Final' && bet_result == 0) ||    # Tie
-        (status == 'Final' && other_hand_count == 0) # No one on the other hand
-        bet.user.points += bet.points
-      end
+#     # bet points back to user
+#     if status == 'Canceled' ||                     # Game Canceled
+#       (status == 'Final' && bet_result == 0) ||    # Tie
+#       (status == 'Final' && other_hand_count == 0) # No one on the other hand
+#       bet.user.points += bet.points
+#     end
 
-      # all poins in game pool divided by winner count back
-      # TODO: points should be divided by ratios
-      if status == 'Final' && bet_result == 1      # Won
-        current_user.points += pool / winner_count
-      end
-    end
-  end
-
-  def resolve_bet(winning_team) #assumes winning_team will be be <Team>, if nil it was a tie else they were the winning team
-    if completed?
-      return  # already did this book-keeping, don't redo it
-    end
-
-      
-    end
-    # figure out if we just refund points.  either no winner, or all bets on one side
-  end
+#     # all poins in game pool divided by winner count back
+#     # TODO: points should be divided by ratios
+#     if status == 'Final' && bet_result == 1      # Won
+#       current_user.points += pool / winner_count
+#     end
+#   end
+# end
 
   # TODO: When & where call this method?
   def cancel_game
