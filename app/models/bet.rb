a662fc15b3560fcc5be79f411cf6af69fc326417
Natalia Moreno <nataliamj@gmail.com>
2016-06-18 22:42:04 +0000

    added profile breakdown and address footer

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,8 +1,8 @@
 class Bet < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :game_team
   has_one :game, through: :game_team
   has_one :stadium, through: :game
   has_one :team, through: :game_team
 
-  belongs_to :user
-  belongs_to :game_team
 end
