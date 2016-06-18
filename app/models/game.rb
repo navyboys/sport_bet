78ae4cb3f0a8541db752ad2917cc6ba77bdaa9b0
navy <navyboys@gmail.com>
2016-06-18 15:05:56 -0700

    call resolve_bets on before_save

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,17 +6,16 @@ class Game < ActiveRecord::Base
 
   belongs_to :stadium
 
-  # TODO: set_result_to_game_team ? 
-  # TODO before_save :resolve_bet()
+  before_save :resolve_bets()
 
   def completed?
-    ['Final', 'Canceled'].include?(status) 
+    ['Final', 'Canceled'].include?(status)
   end
 
   def winner
     game_team_a = game_teams.first
     game_team_b = game_teams.last
-    game_team_a.score > game_team_b.score ? game_team_a : game_team_b 
+    game_team_a.score > game_team_b.score ? game_team_a : game_team_b
   end
 
   def loser
@@ -26,35 +25,35 @@ class Game < ActiveRecord::Base
   def pool
     bets.reduce(0) {|m, bet| m + bet.points}
   end
-  
+
   def tied?
-    game_teams.first.score == game_teams.last.score 
+    game_teams.first.score == game_teams.last.score
   end
   def resolve_bets(winning_team, winscore, losescore) #assumes winning_team will be be <Team>, if nil it was a tie else they were the winning team
-    
+
     return if completed?  # already did this book-keeping, don't redo it
     game_team_a = self.game_teams.first
     game_team_b = self.game_teams.last
     if winning_team
       if game_team_a.team_id == winning_team.id
-        set_game_team_scores(game_team_a, game_team_b, winscore,losescore) 
+        set_game_team_scores(game_team_a, game_team_b, winscore,losescore)
       else
-        set_game_team_scores(game_team_b, game_team_a, winscore,losescore) 
+        set_game_team_scores(game_team_b, game_team_a, winscore,losescore)
       end
-      winning_game_team_id = game_team_a.team_id == winning_team.id ? game_team_a.id : game_team_b.id  
-      winning_bets = self.bets.where(game_team_id: winning_game_team_id) 
+      winning_game_team_id = game_team_a.team_id == winning_team.id ? game_team_a.id : game_team_b.id
+      winning_bets = self.bets.where(game_team_id: winning_game_team_id)
       set_won_bets(winning_bets)
       losing_bets = self.bets.where.not(game_team_id: winning_game_team_id)
       set_lost_bets(losing_bets)
-    else 
+    else
       game_team_a.result = 0
       game_team_a.save!
-      game_team_b.result = 0 
+      game_team_b.result = 0
       game_team_b.save!
       set_tied_bets
-#cancel status will not be implemented in this version      self.status = 'Canceled' if self.game_teams.first.score.nil? 
+#cancel status will not be implemented in this version      self.status = 'Canceled' if self.game_teams.first.score.nil?
     end
-      self.status = 'Final' 
+      self.status = 'Final'
       self.save!
     # figure out if we just refund points.  either no winner, or all bets on one side
   end
@@ -69,7 +68,7 @@ class Game < ActiveRecord::Base
       loseteam.save!
   end
   def set_won_bets(bets)
-    winning_bet_pool = bets.reduce(0) {|m, bet| m + bet.points}   
+    winning_bet_pool = bets.reduce(0) {|m, bet| m + bet.points}
     bets.each do |bet|          #sets bet's profit_points
       decimal_percentage = bet.points.to_f / winning_bet_pool.to_f #check math and decimal if time allows
       winnings = (pool * decimal_percentage).floor
@@ -77,14 +76,14 @@ class Game < ActiveRecord::Base
       bet.save!
     end
   end
-  
+
   def set_lost_bets(bets)
     bets.each do |bet|
       bet.profit_points = 0
       bet.save!
     end
   end
-  
+
   def set_tied_bets
     self.bets.each do |bet|     #everyone gets their points back and 'profit points' are set to bet point amount
       bet.profit_points = bet.points
@@ -101,33 +100,5 @@ class Game < ActiveRecord::Base
     other_hand = (my_result == 1) ? -1 : 1
 
     bet.game.bets.select { |b| b.game_team.result == other_hand }.count
-  end
-
-# def resolve_bet
-#   return unless completed?
-
-#   bets.each do |bet|
-#     bet_result = bet.game_team.result
-
-#     # bet points back to user
-#     if status == 'Canceled' ||                     # Game Canceled
-#       (status == 'Final' && bet_result == 0) ||    # Tie
-#       (status == 'Final' && other_hand_count == 0) # No one on the other hand
-#       bet.user.points += bet.points
-#     end
-
-#     # all poins in game pool divided by winner count back
-#     # TODO: points should be divided by ratios
-#     if status == 'Final' && bet_result == 1      # Won
-#       current_user.points += pool / winner_count
-#     end
-#   end
-# end
-
-  # TODO: When & where call this method?
-  def cancel_game
-    # 1) set status = canelled
-    # 2) refund all bets
-    # 3) ??? set game_team.result to some value ???
   end
 end
