831d11bfc2c1f8e134d3d92e6ab63938380f0df3
maxahn <ahnmaximus@gmail.com>
2016-06-18 16:18:38 -0700

    to really fix bug  on nil error

--- app/models/game.rb
+++ app/models/game.rb
@@ -38,15 +38,16 @@ class Game < ActiveRecord::Base
 
     winning_team = nil
     losing_team = nil
-
-    if game_team_a.try(:score) > game_team_b.try(:score)
-      winning_team = game_team_a
-      losing_team =  game_team_b
-    elsif game_team_a.try(:score) < game_team_b.try(:score)
-      winning_team = game_team_b
-      winning_team = game_team_a
-    else
-      winning_team = nil
+    if game_team_a && game_team_b 
+      if game_team_a.score > game_team_b.score
+        winning_team = game_team_a
+        losing_team =  game_team_b
+      elsif game_team_a.try.score < game_team_b.try.score
+        winning_team = game_team_b
+        winning_team = game_team_a
+      else
+        winning_team = nil
+      end
     end
 
     if winning_team
